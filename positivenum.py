4a0fb09965edf032dbb6ba56392ef88f182a7733
rajeshsawant98 <79257254+rajeshsawant98@users.noreply.github.com>
2021-04-01 08:44:04 +0530

    Created positivenum.py

---
+++ positivenum.py
@@ -0,0 +1,11 @@
+# Python program to print positive Numbers in given range
+
+# list of numbers
+list1 = [45, -15, 74, -97, 69, -7, 17]
+
+# iteration
+for num in list1:
+
+    # condition
+    if num >= 0:
+       print(num, end = " ")
